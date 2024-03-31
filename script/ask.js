7c3a21ab434c18c14609906386d7680fb060f8ad
Herukozen <154478554+MuichiroDev32@users.noreply.github.com>
2024-03-31 08:22:43 +0800

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -7,7 +7,7 @@ module.exports.config = {
 		description: "AI",
 		hasPrefix: false,
 		usages: "{pn} [prompt]",
-		aliases: [],
+		aliases: ["ai"],
 		cooldown: 0,
 };
 
@@ -15,15 +15,17 @@ module.exports.run = async function ({ api, event, args }) {
 		try {
 				const prompt = args.join(" ");
 				if (!prompt) {
-						await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
+						await api.sendMessage("🤖 𝗣𝗵𝗼𝗻𝗸 𝗔𝗜\n\n𝙷𝚎𝚢 𝙸'𝚖 𝚢𝚘𝚞𝚛 𝚟𝚒𝚛𝚝𝚞𝚊𝚕 𝚊𝚜𝚜𝚒𝚜𝚝𝚊𝚗𝚝, 𝚊𝚜𝚔 𝚖𝚎 𝚊 𝚚𝚞𝚎𝚜𝚝𝚒𝚘𝚗.", event.threadID);
 						return;
-				}
-
-				const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+        }
+    api.setMessageReaction("🔎", event.messageID, (err) => {}, true);
+         const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      api.setMessageReaction("✅", event.messageID, (err) => {}, true);
 				const answer = response.data.answer;
 
-				await api.sendMessage(answer, event.threadID);
+				await api.sendMessage('🤖 𝗣𝗵𝗼𝗻𝗸 𝗔𝗜' + answer, event.threadID);
+      api.setMessageReaction("⚠️", event.messageID, (err) => {}, true);
 		} catch (error) {
-				console.error("Error:", error.message);
+				console.error("⚠️ | Error Please Contact the Developer for an Error\n\n-fblink: https://www.facebook.com/jaymar.dev.00", error.message);
 		}
 };
