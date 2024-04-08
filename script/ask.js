be2225b09a5cc2162e098cefaa774cb8bfc8eaae
Herukozen <154478554+MuichiroDev32@users.noreply.github.com>
2024-04-09 06:54:26 +0800

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports.config = {
-		name: "ask",
+		name: "ai",
 		version: 1.0,
 		credits: "OtinXSandip",
 		description: "AI",
@@ -23,7 +23,7 @@ module.exports.run = async function ({ api, event, args }) {
       api.setMessageReaction("✅", event.messageID, (err) => {}, true);
 				const answer = response.data.answer;
 
-				await api.sendMessage('🤖 𝗣𝗵𝗼𝗻𝗸 𝗔𝗜\n\n' + answer, event.threadID);
+				await api.sendMessage('🤖 𝗣𝗵𝗼𝗻𝗸 𝗔𝗜\n\n' + answer + '𝗖𝗿𝗲𝗮𝘁𝗲 𝘆𝗼𝘂𝗿 𝗼𝘄𝗻 𝗮𝘂𝘁𝗼𝗯𝗼𝘁\n\nhttps://autobobot.onrender.com', event.threadID);
 		} catch (error) {
 				console.error("⚠️ | Error Please Contact the Developer for an Error\n\n-fblink: https://www.facebook.com/jaymar.dev.00", error.message);
 		}
