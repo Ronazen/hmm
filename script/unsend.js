a27963cafce67a4568733c7d5ae9d1eb2f939c66
RonaZhai <164463501+RonaZhai@users.noreply.github.com>
2024-04-30 22:39:15 +0800

    Update unsend.js

--- script/unsend.js
+++ script/unsend.js
@@ -3,8 +3,8 @@ module.exports.config = {
   version: "1.0.0",
   role: 0,
   hasPrefix: true,
-  aliases: ['unsent', 'remove', 'rm'],
-  usage: 'Unsent [reply]',
+  aliases: ['huhu', 'remove', 'yie'],
+  usage: 'wagkaiyak [reply]',
   description: "Unsend bot's message",
   credits: 'Deveploper',
   cooldown: 0
@@ -13,7 +13,7 @@ module.exports.run = async function({
   api,
   event
 }) {
-  if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("I can't unsend from other message.", event.threadID, event.messageID);
+  if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("message koba yan para e unsent ko.", event.threadID, event.messageID);
   if (event.type != "message_reply") return api.sendMessage("Reply to bot message", event.threadID, event.messageID);
   return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage("Something went wrong.", event.threadID, event.messageID) : '');
 }
