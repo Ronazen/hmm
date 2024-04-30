60e92ddf0631f04e3b771a5f244c4885a1f8ae59
RonaZhai <164463501+RonaZhai@users.noreply.github.com>
2024-04-30 22:42:21 +0800

    Update antiout.js

--- script/event/antiout.js
+++ script/event/antiout.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = async ({
       if (error) {
         api.sendMessage(`Unable to re-add member ${name} to the group!`, event.threadID);
       } else {
-        api.sendMessage(`Active antiout mode, ${name} OH BAT BUMALIK KA ALIS!`, event.threadID);
+        api.sendMessage(`Active antiout mode, ${name} 𝙖𝙖𝙡𝙞𝙨 𝙠𝙖? 𝙖𝙡𝙖𝙢 𝙢𝙤 𝙣𝙖𝙢𝙖𝙣𝙜 𝙢𝙖𝙢𝙞𝙢𝙞𝙨𝙨 𝙠𝙞𝙩𝙖 𝗸𝗮𝘆𝗮 𝗱𝗶𝘁𝗼 𝗸𝗮𝗹𝗮𝗻𝗴 𝘄𝗮𝗹𝗮𝗻𝗴 𝗮𝗮𝗹𝗶𝘀!`, event.threadID);
       }
     });
   }
