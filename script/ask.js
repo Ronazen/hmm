ad5792d61563248147d83aa6472878790befa21b
RonaZhai <164463501+RonaZhai@users.noreply.github.com>
2024-05-01 19:48:33 +0800

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -25,6 +25,6 @@ module.exports.run = async function ({ api, event, args }) {
 
 				await api.sendMessage('✧⁠   ∩_∩\n✧⁠◝( ⁠ꈍ⁠ᴗ⁠ꈍ)◜⁠✧\n┏━━∪∪━━━━━━━━━━━┓\n♥︎       𝗛𝗘𝗥𝗨𝗚𝗣𝗧       ♥︎\n┗━━━━━━━━━━━━━━━┛\n━━━━━━━━━━━━━━━━━\n' + answer + '\n\n━━━━━━━━━━━━━━━━━' + '\n\n‎‧₊˚✧[ 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐉𝐚𝐲𝐦𝐚𝐫 ]✧˚₊‧:\nhttps://www.facebook.com/jaymar.dev.00', event.threadID);
 		} catch (error) {
-				console.error("⚠️ | Error Please Contact the Developer for an Error\n\n-fblink: https://www.facebook.com/jaymar.dev.00", error.message);
+				console.error("⚠️ | Error Please Contact the Developer for an Error\n\n-fblink: https://web.facebook.com/profile.php 𝗝𝗮𝘆𝗺𝗮𝗿 ", error.message);
 		}
 };
