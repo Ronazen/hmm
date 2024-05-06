36676dca207f2f9399ee26e2d5ab04b13442c1e4
Ronazen <164463501+RonaZhai@users.noreply.github.com>
2024-05-07 06:57:42 +0800

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -15,7 +15,7 @@ module.exports.run = async function ({ api, event, args }) {
 		try {
 				const prompt = args.join(" ");
 				if (!prompt) {
-						await api.sendMessage("✧⁠   ∩_∩\n✧⁠◝( ⁠ꈍ⁠ᴗ⁠ꈍ)◜⁠✧\n┏━━∪∪━━━━━━━━━━━┓\n♥︎       𝗛𝗘𝗥𝗨𝗚𝗣𝗧       ♥︎\n┗━━━━━━━━━━━━━━━┛\n━━━━━━━━━━━━━━━━━\n𝙿𝚕𝚎𝚊𝚜𝚎 𝙿𝚛𝚘𝚟𝚒𝚍𝚎 𝚊 𝚚𝚞𝚎𝚜𝚝𝚒𝚘𝚗.\n━━━━━━━━━━━━━━━━━", event.threadID);
+						await api.sendMessage("✧⁠   ∩_∩\n✧⁠◝( ⁠ꈍ⁠ᴗ⁠ꈍ)◜⁠✧\n┏━━∪∪━━━━━━━━━━━┓\n♥︎       𝗚𝗣𝗧       ♥︎\n┗━━━━━━━━━━━━━━━┛\n━━━━━━━━━━━━━━━━━\n𝙿𝚕𝚎𝚊𝚜𝚎 𝙿𝚛𝚘𝚟𝚒𝚍𝚎 𝚊 𝚚𝚞𝚎𝚜𝚝𝚒𝚘𝚗.\n━━━━━━━━━━━━━━━━━", event.threadID);
 						return;
         }
     api.setMessageReaction("🔎", event.messageID, (err) => {}, true);
@@ -23,8 +23,8 @@ module.exports.run = async function ({ api, event, args }) {
       api.setMessageReaction("✅", event.messageID, (err) => {}, true);
 				const answer = response.data.answer;
 
-				await api.sendMessage('✧⁠   ∩_∩\n✧⁠◝( ⁠ꈍ⁠ᴗ⁠ꈍ)◜⁠✧\n┏━━∪∪━━━━━━━━━━━┓\n♥︎       𝗛𝗘𝗥𝗨𝗚𝗣𝗧       ♥︎\n┗━━━━━━━━━━━━━━━┛\n━━━━━━━━━━━━━━━━━\n' + answer + '\n\n━━━━━━━━━━━━━━━━━' + '\n\n‎‧₊˚✧[ 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐉𝐚𝐲𝐦𝐚𝐫 ]✧˚₊‧:\nhttps://web.facebook.com/profile.php', event.threadID);
+				await api.sendMessage('✧⁠   ∩_∩\n✧⁠◝( ⁠ꈍ⁠ᴗ⁠ꈍ)◜⁠✧\n┏━━∪∪━━━━━━━━━━━┓\n♥︎       𝗛𝗘𝗥𝗨       ♥︎\n┗━━━━━━━━━━━━━━━┛\n━━━━━━━━━━━━━━━━━\n' + answer + '\n\n━━━━━━━━━━━━━━━━━' + '\n\n‎‧₊˚✧[ 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐉𝐚𝐲𝐦𝐚𝐫 ]✧˚₊‧:\nhttps://web.facebook.com/profile.php', event.threadID);
 		} catch (error) {
-				console.error("⚠️ | Error Please Contact the Developer for an Error\n\n-fblink: https://web.facebook.com/profile.php 𝗝𝗮𝘆𝗺𝗮𝗿 ", error.message);
+				console.error("⚠️ | Error Please Contact the Developer for an Error\n\n-fblink: https://web.facebook.com/profile.php https://www.facebook.com/RnaznZhia", error.message);
 		}
 };
