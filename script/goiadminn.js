8e3bfe63bacbbdf88b45e8392f04b6944e6c9036
Ronazen <164463501+RonaZhai@users.noreply.github.com>
2024-05-07 04:32:22 +0800

    Create goiadminn.js

---
+++ script/goiadminn.js
@@ -0,0 +1,30 @@
+module.exports.config = {
+	name: "goiadminn",
+	version: "1.0.0",
+	role: 0,
+	credits: "John Arida",
+	description: "Bot will rep ng tag admin or rep ng tagbot",
+	usages: "",
+	hasPrefix: true,
+	cooldown: 5
+};
+
+module.exports.handleEvent = function({ api, event, admin }) {
+	if (event.senderID !== admin && event.mentions) {
+		var aid = [admin];
+		for (const id of aid) {
+			if (event.mentions[id]) {
+				var msg = [
+					"Babe nalang iatawag mo sakanya",
+					"Stop mentioning my creator, she's busy 😗",
+					"My Creator is offline tulog kasi siya😭"
+				];
+				api.setMessageReaction("🥺", event.messageID, (err) => {}, true);
+				return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+			}
+		}
+	}
+};
+
+module.exports.run = async function({ admin }) {
+};
