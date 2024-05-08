baf112e6dc8b8df661a46af68755befb49a394a9
Ronazen <164463501+RonaZhai@users.noreply.github.com>
2024-05-08 23:19:14 +0800

    Create rank.js

---
+++ rank.js
@@ -0,0 +1,43 @@
+const Canvas = require("canvas");
+const { uploadZippyshare } = global.utils;
+
+const defaultFontName = "BeVietnamPro-SemiBold";
+const defaultPathFontName = `${__dirname}/assets/font/BeVietnamPro-SemiBold.ttf`;
+const { randomString } = global.utils;
+const percentage = total => total / 100;
+
+Canvas.registerFont(`${__dirname}/assets/font/BeVietnamPro-Bold.ttf`, {
+	family: "BeVietnamPro-Bold"
+});
+Canvas.registerFont(defaultPathFontName, {
+	family: defaultFontName
+});
+
+let deltaNext;
+const expToLevel = (exp, deltaNextLevel = deltaNext) => Math.floor((1 + Math.sqrt(1 + 8 * exp / deltaNextLevel)) / 2);
+const levelToExp = (level, deltaNextLevel = deltaNext) => Math.floor(((Math.pow(level, 2) - level) * deltaNextLevel) / 2);
+global.client.makeRankCard = makeRankCard;
+
+module.exports = {
+	config: {
+		name: "rank",
+		version: "1.7",
+		author: "NTKhang",
+		countDown: 5,
+		role: 0,
+		description: {
+			vi: "Xem level của bạn hoặc người được tag. Có thể tag nhiều người",
+			en: "View your level or the level of the tagged person. You can tag many people"
+		},
+		category: "rank",
+		guide: {
+			vi: "   {pn} [để trống | @tags]",
+			en: "   {pn} [empty | @tags]"
+		},
+		envConfig: {
+			deltaNext: 5
+		}
+	},
+
+	onStart: async function ({ message, event, usersData, threadsData, commandName, envCommands, api }) {
+    
