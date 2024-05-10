622f24dad036bcf18b63d1265ded6032f52f6a86
Ronazen <164463501+RonaZhai@users.noreply.github.com>
2024-05-10 11:32:03 +0800

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
 module.exports.config = {
-		name: "ai",
+		name: "ask",
 		version: 1.0,
 		credits: "OtinXSandip",
 		description: "AI",
 		hasPrefix: false,
 		usages: "{pn} [prompt]",
-		aliases: ["ask"],
+		aliases: [],
 		cooldown: 0,
 };
 
@@ -15,24 +15,15 @@ module.exports.run = async function ({ api, event, args }) {
 		try {
 				const prompt = args.join(" ");
 				if (!prompt) {
-						await api.sendMessage("┍━━━━━━━━»•» ⌖ «•«━┑
-
-
-
-┕━»•» ⌖ «•«━━━━━━━━┙", event.threadID);
+						await api.sendMessage("ask me a question.", event.threadID);
 						return;
-        }
-    api.setMessageReaction("🔎", event.messageID, (err) => {}, true);
-         const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
-      api.setMessageReaction("✅", event.messageID, (err) => {}, true);
+				}
+
+				const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
 				const answer = response.data.answer;
 
-				await api.sendMessage('┍━━━━━━━━»•» ⌖ «•«━┑
-
-
-
-┕━»•» ⌖ «•«━━━━━━━━┙' + '\n\n‎‧₊˚✧[ 𝐌𝐚𝐝𝐞 𝐛𝐲 𝐉𝐚𝐲𝐦𝐚𝐫 ]✧˚₊‧:\nhttps://web.facebook.com/profile.php', event.threadID);
+				await api.sendMessage(answer, event.threadID);
 		} catch (error) {
-				console.error("⚠️ | Error Please Contact the Developer for an Error\n\n-fblink: https://web.facebook.com/profile.php https://www.facebook.com/RnaznZhia", error.message);
+				console.error("Error:", error.message);
 		}
 };
