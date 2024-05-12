96d4360a39c02f496dc019690ac9d43fd8087194
Ronazen <164463501+RonaZhai@users.noreply.github.com>
2024-05-12 10:47:04 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,32 +1,27 @@
-const { get } = require('axios');
-
 module.exports.config = {
-	name: "ai",
-	version: "1.0.0",
-	role: 0,
-	hasPrefix: false,
-	credits: "Deku",
-	description: "Talk to AI with continuous conversation.",
-	aliases:  [],
-	usages: "[prompt]",
-	cooldown: 0,
+  name: "ai",
+  version: "30.0.0",
+  hasPermssion: 0,
+  credits: "Choru TikTokers",
+  description: "ChatGpt",
+  commandCategory: "Other",
+  usages: "query",
+  hasPrefix: false,
+  cooldowns: 5
 };
 
-module.exports.run = async function({ api, event, args }) {
-	function sendMessage(msg) {
-		api.sendMessage(msg, event.threadID, event.messageID);
-	}
-
-	if (!args[0]) return sendMessage('system under maintenance.');
-
-	const prompt = args.join(" ");
-	const url = `https://deku-rest-api.replit.app/gpt4?prompt=${encodeURIComponent(prompt)}&uid=${event.senderID}`;
-
-	try {
-		const response = await get(url);
-		const data = response.data;
-		return sendMessage(data.gpt4);
-	} catch (error) {
-		return sendMessage(error.message);
-	}
-}
+module.exports.run = async ({ api, event, args }) => { 
+  const { gpt } = require("gpti");
+  let query = args.join(" ");
+  gpt.v1({
+      prompt: query,
+      model: "GPT-4",
+      markdown: false
+  }, (err, data) => {
+      if(err != null){
+          console.log(err);
+      } else {
+          api.sendMessage(data.gpt, event.threadID, event.messageID);
+      }
+  });
+};
