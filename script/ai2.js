38948da3a4812521d9666ac652694e148c050952
Ronazen <164463501+RonaZhai@users.noreply.github.com>
2024-05-19 07:15:13 +0800

    Update ai2.js

--- script/ai2.js
+++ script/ai2.js
@@ -1,32 +1,39 @@
-const { get } = require('axios');
+const axios = require('axios');
 
 module.exports.config = {
-		name: "ai2",
-		version: "1.0.0",
-		role: 0,
-		hasPrefix: false,
-		credits: "Hazey",
-		description: "Talk to AI with continuous conversation.",
-		aliases:  [],
-		usages: "[prompt]",
-		cooldown: 0,
+  name: 'ai2',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['gpt', 'openai'],
+  description: "An AI command powered by GPT-4",
+  usage: "ai2 [prompt]",
+  credits: 'syempre ikaw😽',
+  cooldown: 3,
 };
 
-module.exports.run = async function({ api, event, args }) {
-		function sendMessage(msg) {
-				api.sendMessage(msg, event.threadID, event.messageID);
-		}
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const input = args.join(' ');
+  if (!input) {
+    api.sendMessage(`bro 
 
-		if (!args[0]) return sendMessage('Please provide a question first.');
+━━━━━━━━━━━━━━━
 
-		const prompt = args.join(" ");
-		const url = `https://hazee-gpt4.onrender.com/gpt?content=${encodeURIComponent(prompt)}`;
-
-		try {
-				const response = await get(url);
-				const data = response.data;
-				return sendMessage(data.gpt);
-		} catch (error) {
-				return sendMessage(error.message);
-		}
-}
+ what-,-`, event.threadID, event.messageID);
+    return;
+  }
+  api.sendMessage(`💌𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜
+━━━━━━━━━━━━━━━━━━\n\n "${input}"`, event.threadID, event.messageID);
+  try {
+    const { data } = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(input)}`);
+    const response = data.response;
+    const credits = '\n𝐜𝐫𝐞𝐝𝐢𝐭𝐬: Ako';
+    api.sendMessage(response + credits, event.threadID, event.messageID);
+  } catch (error) {
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+  }
+};
