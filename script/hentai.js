af3c26bd7cbaa01e2643cfe6d83c69cdd1699554
Ronazen <frender638@gmail.com>
2024-05-22 05:04:44 +0800

    Update hentai.js

--- script/hentai.js
+++ script/hentai.js
@@ -1,130 +1,205 @@
-const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
-const vipData = fs.readFileSync(path.join(__dirname, "vip.json"), "utf8");
-const vipJson = JSON.parse(vipData);
-function isVip(senderID) {
-  return vipJson.permission.includes(senderID.toString());
-}
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = {
-	config: {
-		name: "hentai",
-		aliases: ["hen"],
-		version: "1.0",
-		author: "Kaizenji",
-              countDown: 5,
-		role: 0,
-		shortDescription: "nhentai anime",
-		longDescription: "sends random nhentai",
-		category: "18+ VIP",
-		guide: "{pn}"
-	},
+const MAX_FILE_SIZE_MB = 25;
+const MAX_COUNT = 10;
+let NSFW = true;
 
-	onStart: async function ({ api, event, args, message }) {
-   
-if (!isVip(event.senderID)) {
-      api.sendMessage("You are not a VIP member, contact admin(s) to access VIP command's.", event.threadID, event.messageID);
-      return;
+module.exports.config = {
+  name: 'hentai',
+  aliases: ['civitai', 'civit.ai', 'random-nsfw'],
+  version: '4.0.0',
+  role: 0,
+  credits: 'Kenneth Panio',
+  description: 'Get random uploaded content from civit.ai',
+	hasPrefix: false,
+  usage: '[count] or nsfw',
+  cooldowns: 0,
+};
+
+const getRandomElement = array => array[Math.floor(Math.random() * array.length)];
+
+const getFileExtension = contentType => {
+  const extensions = {
+    'image/jpeg': 'png',
+    'image/png': 'png',
+    'image/gif': 'gif',
+    'image/bmp': 'png',
+    'image/webp': 'png',
+    'video/mp4': 'mp4',
+    'video/webm': 'mp4',
+    'video/quicktime': 'mp4',
+  };
+  return extensions[contentType] || 'unknown';
+};
+
+const downloadAndSaveMedia = async (mediaUrl, index, cacheDir) => {
+  try {
+    const response = await axios.get(mediaUrl, { responseType: 'stream' });
+    const contentType = response.headers['content-type'];
+    const contentExtension = getFileExtension(contentType);
+
+    if (contentExtension === 'unknown') {
+      console.log(`Skipped: ${mediaUrl} - Unknown content type`);
+      return null;
     }
 
-	  var link = [
-"https://i.imgur.com/mNesqCm.jpg",
-"https://i.imgur.com/ChtMmje.jpg",
-"https://i.imgur.com/2oTwWjZ.png",
-"https://i.imgur.com/ZOcTTvR.jpg",
-"https://i.imgur.com/ccLl6Ym.png",
-"https://i.imgur.com/bdQ5fK9.png",
-"https://i.imgur.com/zyqGxHc.png",
-"https://i.imgur.com/1dFL3ZI.png",
-"https://i.imgur.com/SKsfMCE.jpg",
-"https://i.imgur.com/vhinUxm.png",
-"https://i.imgur.com/5QXTkQr.jpg",
-"https://i.imgur.com/cMeEbZe.jpg",
-"https://i.imgur.com/HRDcYSW.jpg",
-"https://i.imgur.com/sp1SnE4.jpg",
-"https://i.imgur.com/Ki3hy27.jpg",
-"https://i.imgur.com/8bxgU0f.jpg",
-"https://i.imgur.com/7iDnRg6.jpg",
-"https://i.imgur.com/shKYOem.png",
-"https://i.imgur.com/MGNxYj4.jpg",
-"https://i.imgur.com/1DTrdAl.jpg",
-"https://i.imgur.com/xAKKpfT.png",
-"https://i.imgur.com/ChtMmje.jpg",
-"https://i.imgur.com/ujo6Yvu.jpg",
-"https://i.imgur.com/dVcSVDs.jpg",
-"https://i.imgur.com/rpdXJEa.jpg",
-"https://i.imgur.com/sToM2cj.png",
-"https://i.imgur.com/wzUFRb9.png",
-"https://i.imgur.com/n378ZMT.png",
-"https://i.imgur.com/A8dKVOY.jpg",
-"https://i.imgur.com/6a7lQUa.jpg",
-"https://i.imgur.com/Oc1XLXW.jpg",
-"https://i.imgur.com/MpQ7qN3.png",
-"https://i.imgur.com/GK2iXnb.jpg",
-"https://i.imgur.com/cwHOc0j.jpg",
-"https://i.imgur.com/NHRGsp6.jpg",
-"https://i.imgur.com/Ia9ugv3.jpg",
-"https://i.imgur.com/CqszBU7.jpg",
-"https://i.imgur.com/JHm5muL.jpg",
-"https://i.imgur.com/509DTo9.png",
-"https://i.imgur.com/JjvzvuI.jpg",
-"https://i.imgur.com/hMljgoy.jpg",
-"https://i.imgur.com/lOn0Vru.jpg",
-"https://i.imgur.com/eDFtiW3.jpg",
-"https://i.imgur.com/JYKT9Uv.jpg",
-"https://i.imgur.com/pG0YTtt.jpg",
-"https://i.imgur.com/tZP3MMz.png",
-"https://i.imgur.com/RM9ukif.jpg",
-"https://i.imgur.com/gMRKKyj.png",
-"https://i.imgur.com/m4WWhGW.png",
-"https://i.imgur.com/3XcAUud.jpg",
-"https://i.imgur.com/ycnn0HV.png",
-"https://i.imgur.com/8VjmAbA.jpg",
-"https://i.imgur.com/vXgbs3j.jpg",
-"https://i.imgur.com/OQ3d05j.jpg",
-"https://i.imgur.com/bXGDK0Y.jpg",
-"https://i.imgur.com/UcvGgH7.jpg",
-"https://i.imgur.com/Ei4JcHB.jpg",
-"https://i.imgur.com/jHcd7na.jpg",
-"https://i.imgur.com/FaN7cnv.png",
-"https://i.imgur.com/hH0of56.png",
-"https://i.imgur.com/pmUdyLW.jpg",
-"https://i.imgur.com/gpsqjlX.jpg",
-"https://i.imgur.com/ohpsz0U.jpg",
-"https://i.imgur.com/Ar8ioUD.png",
-"https://i.imgur.com/WsVWcq2.jpg",
-"https://i.imgur.com/XzQTER5.jpg",
-"https://i.imgur.com/0t1yKln.jpg",
-"https://i.imgur.com/iXAHEqa.jpg",
-"https://i.imgur.com/7yAtWmQ.png",
-"https://i.imgur.com/71f21Ix.png",
-"https://i.imgur.com/x0bcEFl.png",
-"https://i.imgur.com/lH7lYVc.png",
-"https://i.imgur.com/ba8LkRB.png",
-"https://i.imgur.com/FsP5lSm.jpg",
-"https://i.imgur.com/X25um8b.jpg",
-"https://i.imgur.com/rSBexdO.png",
-"https://i.imgur.com/tTTXPgb.png",
-"https://i.imgur.com/MaBiBiD.png",
-"https://i.imgur.com/cZwQjP7.jpg",
-"https://i.imgur.com/iCFsBHc.jpg",
-"https://i.imgur.com/dTZGPiM.jpg",
-"https://i.imgur.com/bQ5xRTG.png",
-"https://i.imgur.com/aFXDHWL.jpg",
-"https://i.imgur.com/tdU2fbU.png",
-"https://i.imgur.com/faRtrw6.png",
-"https://i.imgur.com/id08VTo.png",
-"https://i.imgur.com/heLM5yF.png",
-"https://i.imgur.com/qwRAAlu.png",
-"https://i.imgur.com/7lR2uR3.png",
-"https://i.imgur.com/vL15U0T.jpg",
-"https://i.imgur.com/jhuTb7V.png",        
-]
+    const mediaPath = path.join(cacheDir, `civ_${index + 1}.${contentExtension}`);
+    const fileStream = fs.createWriteStream(mediaPath);
 
-let img = link[Math.floor(Math.random()*link.length)]
-message.reply({body: "Here's your random hentai ❤",
-  attachment: await global.utils.getStreamFromURL(img)
-})
+    response.data.pipe(fileStream);
+
+    return new Promise((resolve, reject) => {
+      fileStream.on('finish', () => resolve({ stream: fs.createReadStream(mediaPath), contentType, contentExtension, path: mediaPath }));
+      fileStream.on('error', reject);
+    });
+
+  } catch (error) {
+    console.error("Error downloading and saving media:", error);
+    return null;
+  }
+};
+
+const getRandomCombinations = () => {
+  const minPage = 1;
+  const maxPage = 31;
+  const randPage = Math.floor(Math.random() * (maxPage - minPage + 1)) + minPage;
+  const randSort = getRandomElement(['Newest', 'Most Reactions', 'Most Comments']);
+  const randPeriod = getRandomElement(['AllTime', 'Year', 'Week', 'Day']);
+
+  return { randPage, randSort, randPeriod };
+};
+
+module.exports.run = async function ({ api, event, args }) {
+  const { threadID, messageID } = event;
+
+  if (args[0] && args[0].toLowerCase() === 'nsfw') {
+    NSFW = !NSFW;
+    const statusMsg = NSFW ? 'NSFW mode is now ON.' : 'NSFW mode is now OFF.';
+    api.sendMessage(statusMsg, threadID, messageID);
+    return;
+  } else { api.sendMessage('🕜 | Finding Delicious Images!...', threadID, messageID);
 }
+
+  const cnt = parseInt(args[0]) || 4;
+
+  if (cnt <= 0 || cnt > MAX_COUNT) {
+    api.sendMessage(`Invalid count. Please provide a count between 1 and ${MAX_COUNT}.`, threadID, messageID);
+    return;
+  }
+
+  const Media = [];
+  const usedCombos = new Set();
+
+  try {
+    const baseUrl = 'https://civitai.com/api/v1/images';
+    const cacheDir = path.join(__dirname, '/cache');
+
+    if (!fs.existsSync(cacheDir)) {
+      fs.mkdirSync(cacheDir);
     }
+
+    for (let i = 0; i < cnt; i++) {
+      let uniqueComboFound = false;
+
+      while (!uniqueComboFound) {
+        const { randPage, randSort, randPeriod } = getRandomCombinations();
+        const comboKey = `${randPage}_${randSort}_${randPeriod}`;
+
+        if (!usedCombos.has(comboKey)) {
+          usedCombos.add(comboKey);
+          uniqueComboFound = true;
+
+          try {
+            const response = await axios.get(baseUrl, {
+              params: {
+                page: randPage,
+                nsfw: NSFW,
+                limit: 18,
+                sort: randSort,
+                period: randPeriod,
+              },
+            });
+
+            if (response.data && response.data.items && response.data.items.length > 0) {
+              const randIndex = Math.floor(Math.random() * response.data.items.length);
+              const randMedia = response.data.items[randIndex];
+              const mediaUrl = randMedia.url;
+
+              const downloadedMedia = await downloadAndSaveMedia(mediaUrl, i, cacheDir);
+
+              if (downloadedMedia) {
+                Media.push(downloadedMedia);
+              }
+            } else {
+           //   api.sendMessage(`No Data Found From Civit.AI`, threadID, messageID);
+            }
+          } catch (error) {
+            console.error("Error fetching data from Civit.AI:", error);
+           // api.sendMessage(`Error fetching data from Civit.AI. Please try again.`, threadID, messageID);
+          }
+        }
+      }
+    }
+
+    const sendMediaMsgs = async (type, attachments) => {
+      if (attachments.length > 0) {
+        const pictureAttachments = attachments.filter(item =>
+          item.contentType.startsWith('image/') && !['video/mp4', 'video/webm'].includes(item.contentType)
+        );
+
+        const videoAttachments = attachments.filter(item =>
+          ['video/mp4', 'video/webm'].includes(item.contentType)
+        );
+
+        const gifAttachments = attachments.filter(item =>
+          item.contentType === 'image/gif'
+        );
+
+        // Send pictures
+        if (pictureAttachments.length > 0) {
+          await api.sendMessage({
+            body: `𝗥𝗔𝗣𝗦𝗔 🤤 - Pictures`,
+            attachment: pictureAttachments.map(item => item.stream),
+          }, threadID, messageID);
+
+          pictureAttachments.forEach(item => fs.unlinkSync(item.path));
+        }
+
+        // Send GIFs
+        if (gifAttachments.length > 0) {
+          await api.sendMessage({
+            body: `𝗥𝗔𝗣𝗦𝗔 🤤 - GIFs`,
+            attachment: gifAttachments.map(item => item.stream),
+          }, threadID, messageID);
+
+          gifAttachments.forEach(item => fs.unlinkSync(item.path));
+        }
+
+        // Send Videos
+        if (videoAttachments.length > 0) {
+          await api.sendMessage({
+            body: `𝗥𝗔𝗣𝗦𝗔 🤤 - Videos`,
+            attachment: videoAttachments.map(item => item.stream),
+          }, threadID, messageID);
+
+          videoAttachments.forEach(item => fs.unlinkSync(item.path));
+        }
+
+        // Delete all files in the cache directory
+        const filesInCache = fs.readdirSync(cacheDir);
+        filesInCache.forEach(file => {
+          const filePath = path.join(cacheDir, file);
+          fs.unlinkSync(filePath);
+        });
+      }
+    };
+
+    await sendMediaMsgs('Media', Media);
+
+  } catch (error) {
+    console.error("General error:", error);
+    api.sendMessage('Server is Down, Try Again Later!', threadID, messageID);
+  }
+};
