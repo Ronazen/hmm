e6285909a78fb5fe2283448392f83d380ecd173f
Ronazen <frender638@gmail.com>
2024-05-22 03:59:57 +0800

    Update unsend.js

--- script/unsend.js
+++ script/unsend.js
@@ -1,22 +1,19 @@
 module.exports.config = {
   name: "unsend",
   version: "1.0.0",
-  role: 0,
-  hasPrefix: true,
-  aliases: ['huhu', 'remove', 'yie'],
-  usage: 'wagkaiyak [reply]',
+  role: 3,
+  hasPrefix: false,
+  aliases: ['unsent', 'remove', 'rm'],
+  usage: 'Unsent [reply]',
   description: "Unsend bot's message",
   credits: 'Deveploper',
   cooldown: 0
 };
-module.exports.run = async function({
+module.exports.run = async function({ api, event, args, admin }) {
   api,
   event
 }) {
-  if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("message koba yan para e unsent ko.", event.threadID, event.messageID);
+  if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("I can't unsend from other message.", event.threadID, event.messageID);
   if (event.type != "message_reply") return api.sendMessage("Reply to bot message", event.threadID, event.messageID);
   return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage("Something went wrong.", event.threadID, event.messageID) : '');
-}
-const rona = "100082748880815";
-   if (!rona.includes(event.senderID))
-   return api.sendMessage("❌ only admin can  use this command.", event.threadID, event.messageID);
+    }
